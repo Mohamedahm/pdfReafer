db48ae251a3166e840be28ac3b61fea7c28a0a89
Mohamedahm <Mohamed_31383@hotmail.com>
2024-04-22 17:25:01 +0400

    Update main.py

--- main.py
+++ main.py
@@ -59,18 +59,28 @@
 # if __name__ == '__main__':
 #     main()
 
-import streamlit as st
+# import streamlit as st
 
+# from langchain.text_splitter import CharacterTextSplitter
+# from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain.vectorstores import FAISS
+# import json
+# import streamlit as st
+# from langchain.llms import OpenAI
+# from langchain.text_splitter import CharacterTextSplitter
+# from langchain.embeddings import OpenAIEmbeddings
+# from langchain.vectorstores import Chroma
+# from langchain.chains import RetrievalQA
+# from langchain.chains.question_answering import load_qa_chain
+# from langchain.llms import OpenAI
+# from googletrans import Translator
+
+import streamlit as st
+from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 import json
-import streamlit as st
-from langchain.llms import OpenAI
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from googletrans import Translator

a5c119c44cb389ad5a5c07561902890a360879c8
Mohamedahm <Mohamed_31383@hotmail.com>
2024-04-22 17:21:26 +0400

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@
 #     main()
 
 import streamlit as st
-
+from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
@@ -71,7 +71,6 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
-import pypdf
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from googletrans import Translator

db2fc756e38daf42d1ceaff0d346b8dfa27b9e13
Mohamedahm <Mohamed_31383@hotmail.com>
2024-04-23 17:25:16 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+from langchain.text_splitter import CharacterTextSplitter  # Assuming this is still part of langchain
+from langchain_community.embeddings.openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import OpenAI
+from langchain_community.chains.question_answering import load_qa_chain
 from googletrans import Translator
 
 def extract_text_from_pdf(pdf_file):
@@ -16,27 +16,21 @@ def extract_text_from_pdf(pdf_file):
     return text
 
 def main():
-    # Set page configuration as the first Streamlit call
     st.set_page_config(page_title='Ask your pdf')
-
     st.header('Ask your pdf')
 
-    # Retrieve the API key from environment variable or secrets
     openai_api_key = os.getenv('OPENAI_API_KEY') or st.secrets.get("OPENAI_API_KEY")
     if not openai_api_key:
         st.error("API key not found. Please set the OPENAI_API_KEY environment variable.")
         return
 
     pdf_file = st.file_uploader("Upload your pdf", type="pdf")
-
     if pdf_file:
         text = extract_text_from_pdf(pdf_file)
-        # Preprocess and split text
         text = text.replace('\n', ' ').join(text.split())
         text_splitter = CharacterTextSplitter(separator='\n', chunk_size=1000, chunk_overlap=200, length_function=len)
         chunks = text_splitter.split_text(text)
 
-        # Create embeddings and knowledge base
         embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
         knowledge_pdf = FAISS.from_texts(chunks, embeddings)
 
@@ -47,7 +41,6 @@ def main():
             chain = load_qa_chain(llm, chain_type="stuff")
             response = chain.run(input_documents=docs, question=user_question)
 
-            # Display the context and answer
             st.write("Question:", user_question)
             st.write("Context:")
             for doc in docs:

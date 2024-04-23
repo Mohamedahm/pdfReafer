dfcba98447f049c6b4df7dd3b2a8b0ac4a03866f
Mohamedahm <Mohamed_31383@hotmail.com>
2024-04-23 16:58:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -16,52 +16,37 @@ def extract_text_from_pdf(pdf_file):
     return text
 
 def main():
-    # Retrieve the API key from environment variable
-    #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
-    st.write(os.environ)
-    
-    
-    openai_api_key = st.secrets["OPENAI_API_KEY"]
-    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-    if not openai_api_key:
-        st.error("API key not found. Please set the OPENAI_API_KEY environment variable.")
-        return
-
+    # Set page configuration as the first Streamlit call
     st.set_page_config(page_title='Ask your pdf')
 
     st.header('Ask your pdf')
+
+    # Retrieve the API key from environment variable or secrets
+    openai_api_key = os.getenv('OPENAI_API_KEY') or st.secrets.get("OPENAI_API_KEY")
+    if not openai_api_key:
+        st.error("API key not found. Please set the OPENAI_API_KEY environment variable.")
+        return
 
     pdf_file = st.file_uploader("Upload your pdf", type="pdf")
 
     if pdf_file:
         text = extract_text_from_pdf(pdf_file)
-
-        # Preprocess text
-        text = text.replace('\n', ' ')  # Remove line breaks
-        text = ' '.join(text.split())  # Remove extra whitespace
-
-        # Split text into chunks
-        text_splitter = CharacterTextSplitter(
-            separator='\n',
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-        )
+        # Preprocess and split text
+        text = text.replace('\n', ' ').join(text.split())
+        text_splitter = CharacterTextSplitter(separator='\n', chunk_size=1000, chunk_overlap=200, length_function=len)
         chunks = text_splitter.split_text(text)
 
-        # Create embeddings
+        # Create embeddings and knowledge base
         embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
         knowledge_pdf = FAISS.from_texts(chunks, embeddings)
 
         user_question = st.text_input("Ask a question about the pdf")
-
         if user_question:
             docs = knowledge_pdf.similarity_search(user_question)
-
             llm = OpenAI(openai_api_key=openai_api_key, model_name='gpt-3.5-turbo-0613')
             chain = load_qa_chain(llm, chain_type="stuff")
             response = chain.run(input_documents=docs, question=user_question)
-            st.write(docs)
+
             # Display the context and answer
             st.write("Question:", user_question)
             st.write("Context:")
